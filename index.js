1610cc8adc6acdced06f284b1942641bbcc00460
Oralddeshapa <max_mus_2010@mail.ru>
2021-03-21 00:31:53 +0300

    basic JS

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ sweets = sweets[0];
 name = name[name.length - 1];
 
 const first = `
-			<div class="img-holder"><img src="images/sharlotka.png" alt="sharlotka"></div>
+			<div class="img-holder"><img src="images/sharlotka.png" alt="cake"></div>
 			<hr/>
 			<div class="name">Your name goes here</div> 
 			<hr/>
@@ -33,32 +33,34 @@ const second = `
 const third = `
 			<div class="input_form">
 				<p><b>URL to cake photo:</b><br>
-	   				<input type="text" size="40" placeholder="Your photo">
+	   				<input class="url_in" type="text" size="40" placeholder="Your photo">
 	  			</p>
 	  			<p><b>Cake name:</b><br>
-	   				<input type="text" size="40" placeholder="Your name goes here">
+	   				<input class="name_in" type="text" size="40" placeholder="Your name goes here">
 	  			</p>
 	  			<p><b>Description:</b><br>
-	   				<textarea name="comment" cols="40" rows="3" placeholder="And here is description"></textarea>
+	   				<textarea class="desc_in" cols="40" rows="3" placeholder="And here is description"></textarea>
 	  			</p>
 	  			<p><b>Picture size:</b><br>
-	  				<input type="range" min="1" max="100" value="50">
+	  				<input class="range_in" type="range" min="50" max="100" value="100">
 	  			</p>
 	  			<p><b>Ingridients:</b><br>
-	  				<select size="1">
+	  				<select class="ing_option" size="1">
 				      <option value="1">Water</option>
 				      <option value="2">Flavor</option>
 				    </select>
-				    <input type="text" size="40">
+				    <button class="add_ing">ADD</button>
+				    <input class="ing_in" type="text" size="40">
 				</p>
 				<p><b>Steps:</b><br>
-	  				<select size="1">
+	  				<select class="steps" size="1">
 				      <option value="1">Boil water</option>
 				      <option value="2">Mix water with flavor</option>
 				    </select>
-				    <textarea name="comment" cols="40" rows="3"></textarea>
+				    <button class="add_step">ADD</button>
+				    <textarea class="step_in" cols="40" rows="3"></textarea>
 				</p>
-				<button>Save</button>
+				<button class="save" >Save</button>
 			</div>
 			`
 
@@ -84,4 +86,77 @@ customize.onclick = function (){
    		space.appendChild(Things[i]);
    } 
 
+   document.querySelector('.input_form .name_in').oninput = function() {
+    document.querySelector('.form_element .name').innerHTML = document.querySelector('.input_form .name_in').value;
+  };
+
+  document.querySelector('.input_form .desc_in').oninput = function() {
+    document.querySelector('.form_element .description').innerHTML = document.querySelector('.input_form .desc_in').value;
+  };
+
+  document.querySelector('.input_form .url_in').oninput = function() {
+    document.querySelector('.form_element .img-holder img').src = document.querySelector('.input_form .url_in').value;
+  	document.querySelector('.ing_custom').style.backgroundImage = "url(" + document.querySelector('.input_form .url_in').value + ")";
+  };
+
+  document.querySelector('.range_in').oninput = function() {
+    document.querySelector('.form_element .img-holder img').style.height = String(Number(document.querySelector('.range_in').value) / 3) + "vh";
+  	document.querySelector('.form_element .img-holder img').style.width = document.querySelector('.range_in').value + "%";
+  	let val = document.querySelector('.range_in').value;
+  	document.querySelector('.ing_custom').style.backgroundSize = val + "% " + val + "%";
+  };
+
+  document.querySelector('.add_ing').onclick = function() {
+    let select =  document.querySelector('.ing_option');
+    let ing = document.querySelector('.ing_list');
+    let val = document.querySelector('.ing_in').value;
+    let option = document.createElement('option');
+    let li = document.createElement('li');
+    let li_list = document.querySelector('.ing_list').getElementsByTagName('li');
+    for (let i = 0; i < li_list.length; i ++)
+    {
+    	 if(li_list[i].innerHTML == val)
+    	 	return;
+    }
+    if (val != "")
+    {
+	    li.value = val;
+	    li.innerHTML = val;
+	    option.value = val;
+	    option.innerHTML = val;
+	    select.appendChild(option);
+	    ing.appendChild(li);
+	}
+  };
+
+  document.querySelector('.add_step').onclick = function() {
+    let select =  document.querySelector('.steps');
+    let ing = document.querySelector('.recipe');
+    let val = document.querySelector('.step_in').value;
+    let option = document.createElement('option');
+    let li = document.createElement('li');
+    let li_list = document.querySelector('.recipe').getElementsByTagName('li');
+    for (let i = 0; i < li_list.length; i ++)
+    {
+    	 if(li_list[i].innerHTML == val)
+    	 	return;
+    }
+    if (val != "")
+    {
+	    li.value = val;
+	    li.innerHTML = val;
+	    option.value = val;
+	    option.innerHTML = val;
+	    select.appendChild(option);
+	    ing.appendChild(li);
+	}
+  };
+
+  document.querySelector('.save').onclick = function() {
+  	document.getElementsByClassName('content')[0].style.marginTop = "0";
+  	sweets.classList.remove('blur');
+    footer.classList.remove('blur');
+  	space.parentNode.removeChild(space);
+  	
+  }
 }
