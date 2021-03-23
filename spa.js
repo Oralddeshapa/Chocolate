afe934d8a3cfd75d571aac196052829decdf5c26
Oralddeshapa <max_mus_2010@mail.ru>
2021-03-23 19:31:29 +0300

    SPA_alpha

---
+++ spa.js
@@ -0,0 +1,80 @@
+//firebase deploy --only hosting
+
+class Block {
+	constructor(name, text) {
+		this.name = name;
+		this.text = text;
+	}
+}
+
+class BlocksManager {
+	constructor() {
+		this.blocksList = [
+		'index_body',
+		'menu_body',
+		'recipes_body'
+		];
+
+		this.blocks = [];
+		this.prepareBlocks();
+	}
+
+	async prepareBlocks(){
+		for (let name of this.blocksList){
+			const block = await fetch('./blocks/${name}.hmtl');
+			const content = await block.text();
+			this.blocks.push(new Block(name, content));
+		}
+	}
+}
+
+class SPA {
+
+  constructor() {
+      this.init();
+      this.changeBack();
+      this.state = {
+         currentSlide: null;
+      };
+  }
+
+  initialize() {
+    window.history.replaceState(this.state, null, "");
+  }
+
+  pushBlock(block) {
+        this.state.currentSlide = slide;
+        window.history.pushState(this.state, null, "");
+        this.render();
+    }
+
+  changeBack() {
+    let that = this;
+    window.onpopstate = function(event) {
+      if (event.state) 
+        that.state = event.state;
+      that.render();
+    }
+  }
+
+  render() {
+    let oldBlock = document.getElementById('old-block');
+    if (oldBlock != null) {
+      oldBlock.parentNode.removeChild(oldBlock);
+    }
+    let newBlock = document.createElement('div');
+    newBlock.id = 'old-block';
+    if (this.state.currentBlock != undefined) {
+      newBlock.innerHTML = this.state.currentBlock.htmlContent;
+      document.body.insetBefore(newBlock, document.getElementsByTagName('footer')[0]);
+    }
+    else 
+      alert('What?');
+  }
+
+}
+
+const BB = new BlocksManager();
+const Spa = new SPA();
+
+Spa.pushBlock(BB.blocksList[0]);
