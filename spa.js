418acc061acb673abb78e438339f53297286fa43
Oralddeshapa <max_mus_2010@mail.ru>
2021-03-23 20:05:25 +0300

    hot

--- spa.js
+++ spa.js
@@ -21,7 +21,7 @@ class BlocksManager {
 
 	async prepareBlocks(){
 		for (let name of this.blocksList){
-			const block = await fetch('./blocks/${name}.hmtl');
+			const block = await fetch('./blocks/${name}.html');
 			const content = await block.text();
 			this.blocks.push(new Block(name, content));
 		}
